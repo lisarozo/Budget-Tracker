d9b83833c5dc3ce604e6aec68472efe7027a3205
Lisa Rozo <erozo190@gmail.com>
2021-12-18 10:09:56 -0600

    added webpack

---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+const APP_PREFIX = 'FoodFest-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+  ];
+
+self.addEventListener('install', function (e) {
+
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        })
+        
+self.addEventListener('fetch', function (e) {
+ console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+          
+            )
+          })   
